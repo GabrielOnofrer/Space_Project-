2aa38d5580d9bedd04eaf64e2c36946d3744d529
GabrielOnofrer <gabrielonofrer@gmail.com>
2023-06-27 21:43:54 -0300

    Vfinal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from tkinter import simpledialog
+import json
+import os
 
 pygame.init()
 tamanho = (800, 500)
@@ -8,9 +10,13 @@ preto = (0, 0, 0)
 clock = pygame.time.Clock()
 tela = pygame.display.set_mode(tamanho)
 pygame.display.set_caption("Projeto Space")
-
+fonte = pygame.font.Font(None, 20)
+texto_salvar = fonte.render("F10 para salvar", True, branco)
+texto_carregar = fonte.render("F11 para carregar", True, branco)
+texto_limpar = fonte.render("F12 para limpar", True, branco)
 running = True
 icone = pygame.image.load("space.png")
+pygame.display.set_icon(icone)
 fundo = pygame.image.load("bg.jpg")
 pygame.display.set_icon(icone)
 pygame.mixer.music.load("Space_Machine_Power.mp3")
@@ -19,8 +25,34 @@ circles = []
 lines = []
 estrelas = {}
 
+def salvar_dados():
+    with open("dados.json", "w") as file:
+        json.dump(estrelas, file)
+
+def carregar_dados():
+    global estrelas
+    estrelas.clear()
+    circles.clear()
+    lines.clear()
+    if os.path.isfile("dados.json") and os.path.getsize("dados.json") > 0:
+        with open("dados.json", "r") as file:
+            estrelas = json.load(file)
+
+def limpar_dados():
+    global estrelas
+    estrelas.clear()
+    circles.clear()
+    lines.clear()
+    if os.path.isfile("dados.json"):
+        os.remove("dados.json")
+
+carregar_dados()
+
 while running:
     tela.blit(fundo, (0, 0))
+    tela.blit(texto_salvar, (10, 10))
+    tela.blit(texto_carregar, (10, 30))
+    tela.blit(texto_limpar, (10, 50))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -39,7 +71,7 @@ while running:
         x, y, item = circle
         pygame.draw.circle(tela, branco, (x, y), 20)
         font = pygame.font.Font(None, 20)
-        text = font.render(item, True, preto)
+        text = font.render(item, True, branco)
         tela.blit(text, (x + 25, y - 10))
 
     if len(circles) >= 2:
@@ -51,13 +83,14 @@ while running:
     pygame.display.update()
     clock.tick(40)
 
-    
     keys = pygame.key.get_pressed()
     if keys[pygame.K_F10]:
-        
-        with open("dados.txt", "w") as file:
-            file.write("Estrelas:\n")
-            for estrela, coordenadas in estrelas.items():
-                file.write(f"Nome: {estrela}, Coordenadas: {coordenadas}\n")
+        salvar_dados()
+    elif keys[pygame.K_F11]:
+        carregar_dados()
+        for item, (x, y) in estrelas.items():
+            circles.append((x, y, item))
+    elif keys[pygame.K_F12]:
+        limpar_dados()
 
 pygame.quit()
